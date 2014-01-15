011d72812e94be1f52cb68a6b6dd89332c4e6bcc
cyndiWade <zhanglin492103904@qq.com>
2014-01-15 15:47:31 +0800

    wade 15:47

--- config.inc.php
+++ config.inc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 if (!defined('THINK_PATH'))	exit();
 
@@ -15,22 +14,5 @@ return array (
     'DB_FIELDS_CACHE'       => true,       
     'DB_CHARSET'            => 'utf8',     
 );
-=======
-<?php 
-if (!defined('THINK_PATH'))	exit();
 
-return array (
-	/* 数据库配置 */
-    'DB_TYPE'               => 'mysql',    
-	'DB_HOST'               => 'localhost',
-	'DB_NAME'               => 'lehuo',      
-	'DB_USER'               => 'root',    
-	'DB_PWD'                => '514591',  
-	'DB_PORT'               => '3306', 
-	'DB_PREFIX'             => 'lh_',  
-    'DB_FIELDTYPE_CHECK'    => false,    
-    'DB_FIELDS_CACHE'       => true,       
-    'DB_CHARSET'            => 'utf8',     
-);
->>>>>>> 875753ed9bff50cc845fe9ec340485710b57dc3a
 ?>
